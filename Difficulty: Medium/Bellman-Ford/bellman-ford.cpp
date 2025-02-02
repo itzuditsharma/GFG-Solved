f3a5161db7326cd46c2b7e78a38818e8a94f4cd9
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-02 10:40:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
@@ -21,26 +21,27 @@ class Solution {
     vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
         vector<int> dist(V, 1e8);
         dist[src] = 0;
+        
         for(int i = 0; i < V-1; i++){
             for(auto it : edges){
                 int u = it[0];
                 int v = it[1];
                 int wt = it[2];
                 
-                if(dist[u]!=1e8 && dist[u] + wt < dist[v]){
+                if(dist[u] != 1e8 && dist[v] > dist[u] + wt){
                     dist[v] = dist[u] + wt;
-                    
                 }
             }
         }
-        
-        for(auto it : edges){
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
-            
-            if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
-                return {-1};
+        for(int i = 0; i < V-1; i++){
+            for(auto it : edges){
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                
+                if(dist[u] != 1e8 && dist[v] > dist[u] + wt){
+                    return {-1};
+                }
             }
         }
         
