6f8de733a2dd5fc032ee2cbca8a3a123cda60223
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-14 09:46:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -5,30 +5,31 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function template for C++
 
 class Solution {
   public:
     bool f(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
-        if(target == 0) return true;
-        if(ind==0) return (target == arr[ind]);
+        if(target == 0) return true; 
+        if(ind == 0) return arr[0] == target;
         
         if(dp[ind][target] != -1) return dp[ind][target];
         
         bool nottake = f(ind-1, target, arr, dp);
         bool take = false;
+        if(target >= arr[ind])take = f(ind-1, target - arr[ind], arr, dp);
         
-        if(target >= arr[ind]) take = f(ind-1, target -arr[ind], arr, dp);
-        
-        return dp[ind][target] = take | nottake;
+        return dp[ind][target] = nottake | take;
     }
   
     bool isSubsetSum(vector<int>& arr, int target) {
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
         return f(n-1, target, arr, dp);
     }
 };
+
 
 //{ Driver Code Starts.
 
