e894daf39ae2276c447a750ade343da919394dd2
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-19 15:49:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -6,42 +6,41 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    
-    int f(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+    int f(int ind, int target, vector<int>&arr, vector<vector<int>> &dp){
         if(ind == 0){
             if(target == 0 && arr[0] == 0) return 2;
             if(target == 0 || arr[0] == target) return 1;
             return 0;
         }
-        
         if(dp[ind][target] != -1) return dp[ind][target];
         
-        int nottake = f(ind -1, target, arr, dp);
+        int nottake = f(ind - 1, target, arr, dp);
         int take = 0;
-        if(arr[ind] <= target) take = f(ind -1, target -arr[ind], arr, dp);
+        if(target >= arr[ind]) take = f(ind -1, target - arr[ind], arr, dp);
         
         return dp[ind][target] = take + nottake;
     }
   
     int countPartitions(vector<int>& arr, int d) {
-        // s1 - s2 =d
-        // totalsum -s2 -s2 = d
-        // totalsum -d = 2s2
-        // s2 = (totalsum - d) / 2
+        // s1 -s2 = d
+        // totalsum - s2 -s2 = d
+        // totalsum -2s2 = d
+        // s2 = (totalsum -d)/2 
         
         int n = arr.size();
         int totalsum = 0;
         for(int i = 0; i < n; i++) totalsum += arr[i];
-        if(totalsum - d < 0 || (totalsum - d) % 2 ==1) return 0;
+        if(totalsum - d < 0 || (totalsum-d) % 2 == 1) return 0; 
         int s2 = (totalsum - d) / 2;
-        
         vector<vector<int>> dp(n, vector<int>(s2+1, -1));
+        // s2 is the target 
         return f(n-1, s2, arr, dp);
-        
     }
 };
+
 
 //{ Driver Code Starts.
 
