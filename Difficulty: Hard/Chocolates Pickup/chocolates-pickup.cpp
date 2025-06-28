0cc243490e6f4e241703684c62799788bd198fcb
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-06-28 11:34:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
@@ -1,70 +1,38 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int f(int i, int j1, int j2,int n, int m, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp){
-        // Out of bounds 
-        if(j1 < 0 || j2 < 0 || j1 >= m || j2 >= m) return -1e8;
-        // Destination base case 
-        if(i == n - 1){
+    int f(int row, int j1, int j2, int n, int m, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp){
+        if(j1 < 0 || j2 < 0 || j1 >=m || j2 >= m) return -1e8;
+        if(row == n-1){
             if(j1 == j2){
-                return grid[i][j1];
+                return grid[row][j1];
             }else{
-                return grid[i][j1] + grid[i][j2];
+                return grid[row][j1] + grid[row][j2];
             }
         }
         
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        if(dp[row][j1][j2] != -1) return dp[row][j1][j2];
         
-        // Explore all paths of Alice and Bob simultaneously
-        int maxi = -1e8;
+        int max_chocolates = -1e8;
         for(int dj1 = -1; dj1 <= 1; dj1++){
             for(int dj2 = -1; dj2 <= 1; dj2++){
                 int value = 0;
-                if(j1 == j2) value = grid[i][j1];
-                else value = grid[i][j1] + grid[i][j2];
-                value = value + f(i+1, j1 + dj1, j2 + dj2, n, m, grid, dp);
-                
-                maxi = max(maxi, value);
+                if(j1 == j2){
+                    value += grid[row][j1];
+                }else{
+                    value += grid[row][j1] + grid[row][j2];
+                }
+                value = value + f(row + 1, j1 + dj1, j2 + dj2, n, m, grid, dp);
+                max_chocolates = max(max_chocolates, value);
             }
         }
-        return dp[i][j1][j2] = maxi;
+        
+        return dp[row][j1][j2] = max_chocolates;
+        
     }
   
     int solve(int n, int m, vector<vector<int>>& grid) {
+        // code here
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
         return f(0, 0, m-1, n, m, grid, dp);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> grid;
-        for (int i = 0; i < n; i++) {
-            vector<int> temp;
-            for (int j = 0; j < m; j++) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            grid.push_back(temp);
-        }
-
-        Solution obj;
-        cout << obj.solve(n, m, grid) << "\n";
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
