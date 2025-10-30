1bb759a824a7003110f77d0276e4924aa6c15102
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-10-30 11:25:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
@@ -1,19 +1,16 @@
 from collections import defaultdict
 class Solution:
     def totalElements(self, arr):
-        n = len(arr)
-        count = 0
-        
         l = 0
         r = 0
-        
+        count = 0
         mapp = defaultdict(int)
         
-        while r < n:
+        while r < len(arr):
             mapp[arr[r]] += 1
             while len(mapp) > 2:
                 mapp[arr[l]] -= 1
-                if(mapp[arr[l]] == 0):
+                if mapp[arr[l]] == 0:
                     del mapp[arr[l]]
                 l+=1
             
@@ -21,4 +18,3 @@ class Solution:
             r+=1
         
         return count
-                
