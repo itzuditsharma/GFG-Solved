b003ac8eabb54b44259950502f1c8cb08ad4072a
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-11-13 12:52:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
@@ -3,18 +3,19 @@ class Solution:
     def totalElements(self, arr):
         l = 0
         r = 0
-        count = 0
         mapp = defaultdict(int)
+        maxlen = 0
         
         while r < len(arr):
             mapp[arr[r]] += 1
+            
             while len(mapp) > 2:
                 mapp[arr[l]] -= 1
                 if mapp[arr[l]] == 0:
                     del mapp[arr[l]]
                 l+=1
             
-            count = max(count, r-l+1)
+            maxlen = max(maxlen, r-l+1)
             r+=1
-        
-        return count
+            
+        return maxlen
