968a563ac65d56f4182638f437dcc5238aa8f7b0
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-02-25 19:15:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.py
+++ Difficulty: Medium/Count Inversions/count-inversions.py
@@ -1,44 +1,44 @@
 class Solution:
-    def merge(self, arr, low, mid, high):
+    def merge(self, low, mid, high, arr):
         temp = []
         count = 0
         left = low
-        right = mid + 1
+        right = mid+1
         
         while left <= mid and right <= high:
             if arr[left] <= arr[right]:
                 temp.append(arr[left])
-                left += 1
+                left+=1
             else:
                 temp.append(arr[right])
                 count += (mid - left) + 1
-                right +=1
+                right += 1
         
         while left <= mid:
             temp.append(arr[left])
-            left += 1
+            left+=1
         
         while right <= high:
             temp.append(arr[right])
-            right += 1
-            
-        arr[low:high+1] = temp
+            right+=1
+        
+        arr[low : high+1] = temp
         
         return count
-        
     
-    def mergeSort(self, arr, low, high):
+    def mergeSort(self, low, high, arr):
         count = 0
         if low >= high:
             return count
-        mid = low + (high - low)// 2
+            
+        mid = (low + high)//2
         
-        count += self.mergeSort(arr, low, mid)
-        count += self.mergeSort(arr, mid + 1, high)
-        count += self.merge(arr, low, mid, high)
+        count += self.mergeSort(low, mid, arr)
+        count += self.mergeSort(mid+1, high, arr)
+        count += self.merge(low, mid, high, arr)
         
         return count
     
     def inversionCount(self, arr):
-        n = len(arr)
-        return self.mergeSort(arr, 0, n-1)
+        n =  len(arr)
+        return self.mergeSort(0, n-1, arr)
